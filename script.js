6bab9c957d2ac7ae5e0c49d20dbd3316cea4b61f
Aria-707 <arirg100@gmail.com>
2025-07-21 20:03:58 -0500

    cambiando variables de entorno

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDF3JbaIXh7niaPJcshu1KnJl9dNGPCUHk",
   authDomain: "asistenciaconreconocimiento.firebaseapp.com",
   projectId: "asistenciaconreconocimiento",
-  // etc. Asegúrate de copiar todos los campos desde Firebase Console
+  storageBucket: "asistenciaconreconocimiento.firebasestorage.app",
+  messagingSenderId: "973062515655",
+  appId: "1:973062515655:web:d249787d1c89a9f27147e2",
+  measurementId: "G-PSGDE2EG85"
 };
 
 firebase.initializeApp(firebaseConfig);
